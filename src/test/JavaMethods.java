909948b8e288ab5c5dc1f88e7cee9f7de7f012a6
NitheeshNik <146535374+NitheeshNik@users.noreply.github.com>
2026-08-07 17:38:26 +0530

    java methods

--- src/test/JavaMethods.java
+++ src/test/JavaMethods.java
@@ -1,22 +1,30 @@
 package test;
 
 public class JavaMethods {
-	void addition() { // method withou argument
+	void addition() { // method without argument
 		int a = 10;
 		int b= 10;
 		int c = a+b;
 		System.out.println("Result - " + c);
 	}
 	
-	void sub(int a,int b) {
+	void sub(int a,int b) { // method with argument
 		int c= a-b;
-		System.out.println("Result" + c);
+		System.out.println("Result " + c);
+	}
+	int add(int a , int b) { // method with return type
+		int c = a+b;
+		return c;
+	}
+	void str(String a, String b, String c) { // method with 3 argument 
+		System.out.println("Result -" + a+ b+ c);
 	}
 	public static void main(String[] args) {
 		JavaMethods caller = new JavaMethods();
 		caller.addition();
 		caller.sub(20, 25);
-		
+		System.out.println(caller.add(1, 4));
+		caller.str("X", "Y", "Z");
 	}
 
 }
