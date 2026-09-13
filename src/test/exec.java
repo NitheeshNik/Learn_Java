56a0339329296d5f7129689f848a614bba47bdea
NitheeshNik <146535374+NitheeshNik@users.noreply.github.com>
2026-09-13 16:57:43 +0530

    exception handling java

---
+++ src/test/exec.java
@@ -0,0 +1,12 @@
+class exec {
+
+    public static void main(String[] args) {
+        try {
+            int a = 10 / 0;
+            System.out.println(a);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        System.out.println("Rest the code");
+    }
+}
