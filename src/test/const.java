2c19eacb15b2c3a4eb03667f84d38570f2ba17a6
NitheeshNik <146535374+NitheeshNik@users.noreply.github.com>
2026-09-13 13:07:59 +0530

    small changes

--- src/test/const.java
+++ src/test/const.java
@@ -2,14 +2,13 @@ class Const {
     int a;
     String b;
 
-    void display() {
+    Const() {
         System.out.println(a + ", " + b);
     }
 
     public static void main(String args[]) {
 
         Const obj = new Const();
-        obj.display(); // default constructor assign a the a,b automatically.
 
     }
 
