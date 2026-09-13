059730893ac5bcaf170f24b44827918a4b00cd12
NitheeshNik <146535374+NitheeshNik@users.noreply.github.com>
2026-09-13 13:23:50 +0530

    i lean sting handinling

---
+++ src/test/String_ex.java
@@ -0,0 +1,17 @@
+class String_ex {
+
+    public static void main(String[] args) {
+        String s = "JINI";
+        String b = "NIK";
+        System.out.println(s.toUpperCase());// all upper
+        System.out.println(s.toLowerCase());// all lower
+        System.out.println(s);
+        System.out.println(s.concat(" " + b));// concadination strings
+        // Sub String
+        System.out.println(s.substring(2));// substring
+        System.out.println(s.replace("chello", "JII"));
+        System.out.println(s.length());
+        System.out.println(s.charAt(3)); // only print spacific character
+
+    }
+}
