4c2188e1bc0fd37187e7adf4cb13b40352cb8835
NitheeshNik <146535374+NitheeshNik@users.noreply.github.com>
2026-09-14 16:58:15 +0530

    hashmap

---
+++ src/test/hash_ex.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class hash_ex {
+    public static void main(String[] args) {
+        HashMap<Integer, String> map = new HashMap<Integer, String>(); // Creating HashMap
+
+        map.put(1, "gowtham");
+        map.put(2, "nik");
+        map.put(3, "saro");
+        map.put(1, "jini");
+
+        System.out.println("Iterating Hashmap...");
+        for (Map.Entry m : map.entrySet()) {
+            System.out.println(m.getKey() + " " + m.getValue());
+        }
+    }
+}
